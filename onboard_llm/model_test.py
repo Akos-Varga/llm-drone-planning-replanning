9a25d5ae2ad27bbd6578caeb32f429ef5002b88c
Akos-Varga <86043371+Akos-Varga@users.noreply.github.com>
2026-04-09 14:29:37 +0200

    Using telemetry dataclass

--- onboard_llm/model_test.py
+++ onboard_llm/model_test.py
@@ -1,14 +1,14 @@
 import random
-from task_admission_llm import onboard_task_admission
+from task_admission_llm import onboard_task_admission, Telemetry
 
-def task_admission(max_flight, bat_perc, bat_health, link_qual, drone_state, flight_dur, task_dur):
+def task_admission(t: Telemetry):
     OPERATIONAL_STATES = {"LANDED", "LANDING", "TAKINGOFF", "HOVERING", "FLYING"}
 
-    drone_state_ok = drone_state in OPERATIONAL_STATES
-    link_qual_ok = link_qual > 3
+    drone_state_ok = t.drone_state in OPERATIONAL_STATES
+    link_qual_ok = t.link_qual > 3
 
-    available_time = max_flight * (bat_perc / 100) * (bat_health / 100)
-    required_time = flight_dur + task_dur
+    available_time = t.max_flight * (t.bat_perc / 100) * (t.bat_health / 100)
+    required_time = t.flight_dur + t.task_dur
 
     SAFETY_MARGIN = 1.0 
     flight_ok = available_time * SAFETY_MARGIN >= required_time
@@ -41,13 +41,46 @@ def generate_values():
     flight_dur = int(random.triangular(1, 10, 8))
     task_dur = int(random.triangular(1, 10, 8))
 
-    return max_flight, bat_perc, bat_health, link_qual, drone_state, flight_dur, task_dur
+    return Telemetry(
+        max_flight=max_flight,
+        bat_perc=bat_perc,
+        bat_health=bat_health,
+        link_qual=link_qual,
+        drone_state=drone_state,
+        flight_dur=flight_dur,
+        task_dur=task_dur
+    )
+
+def test_helper(model: str, t: Telemetry, results, error_type, inference_times):
+    print("======================================\n\n")
+    print(f"""
+        max_flight_time: {t.max_flight}
+        battery_percentage: {t.bat_perc}
+        battery_health: {t.bat_health}
+        link_quality: {t.link_qual}
+        drone_state: {t.drone_state}
+        flight_duration: {t.flight_dur}
+        task_duration: {t.task_dur}
+        """)
+    print(f"TEST CASE: {error_type}\n")
+    results[error_type]['total'] += 1
+    accept, reason, error, inference_time = onboard_task_admission(model, t)
+    inference_times.append(inference_time)
+    print(f"Inference time: {inference_time:.2f}")
+    print(f"ACCEPT | {reason}\n\n") if accept else print(f"REJECT | {reason}\n\n")
+    if error_type == "accept":
+        if accept and not error:
+            results[error_type]['correct'] += 1
+            return
+    else:
+        if not accept and not error:
+            results[error_type]['correct'] += 1
 
 
 if __name__ == "__main__":
-    model = "qwen3:4b"
+    model = "qwen3:1.7b"
     inference_times = []
-    TEST_NUM = 20
+    TEST_NUM = 10
     random.seed(21)
     results = {
         "accept": {"correct": 0, "total": 0},
@@ -60,91 +93,23 @@ if __name__ == "__main__":
            or results['flight_time_err']['total'] < TEST_NUM 
            or results['accept']['total'] < TEST_NUM
            ):
-        max_flight, bat_perc, bat_health, link_qual, drone_state, flight_dur, task_dur = generate_values()
-        drone_state_ok, link_qual_ok, flight_ok = task_admission(max_flight, bat_perc, bat_health, link_qual, drone_state, flight_dur, task_dur)
+        t = generate_values()
+        drone_state_ok, link_qual_ok, flight_ok = task_admission(t)
 
         if not drone_state_ok and link_qual_ok and flight_ok and results['drone_state_err']['total'] < TEST_NUM:
-            print("======================================\n\n")
-            print(f"""
-                max_flight_time: {max_flight}
-                battery_percentage: {bat_perc}
-                battery_health: {bat_health}
-                link_quality: {link_qual}
-                drone_state: {drone_state}
-                flight_duration: {flight_dur}
-                task_duration: {task_dur}
-                """)
-            print("DRONE STATE ERROR\n\n")
-            results['drone_state_err']['total'] += 1
-            accept, reason, error, inference_time = onboard_task_admission(model, max_flight, bat_perc, bat_health, link_qual, drone_state, flight_dur, task_dur)
-            inference_times.append(inference_time)
-            print(f"Inference time: {inference_time:.2f}")
-            print(f"{reason}\n\n")
-            if not accept and not error:
-                results['drone_state_err']['correct'] += 1
+            test_helper(model, t, results, 'drone_state_err', inference_times)
             continue
 
         if drone_state_ok and not link_qual_ok and flight_ok and results['link_qual_err']['total'] < TEST_NUM:
-            print("======================================\n\n")
-            print(f"""
-                max_flight_time: {max_flight}
-                battery_percentage: {bat_perc}
-                battery_health: {bat_health}
-                link_quality: {link_qual}
-                drone_state: {drone_state}
-                flight_duration: {flight_dur}
-                task_duration: {task_dur}
-                """)
-            print("LINK QUALITY ERRROR\n\n")
-            results['link_qual_err']['total'] += 1
-            accept, reason, error, inference_time = onboard_task_admission(model, max_flight, bat_perc, bat_health, link_qual, drone_state, flight_dur, task_dur)
-            inference_times.append(inference_time)
-            print(f"Inference time: {inference_time:.2f}")
-            print(f"{reason}\n\n")
-            if not accept and not error:
-                results['link_qual_err']['correct'] += 1
+            test_helper(model, t, results, 'link_qual_err', inference_times)
             continue
 
         if drone_state_ok and link_qual_ok and not flight_ok and results['flight_time_err']['total'] < TEST_NUM:
-            print("======================================\n\n")
-            print(f"""
-                max_flight_time: {max_flight}
-                battery_percentage: {bat_perc}
-                battery_health: {bat_health}
-                link_quality: {link_qual}
-                drone_state: {drone_state}
-                flight_duration: {flight_dur}
-                task_duration: {task_dur}
-                """)
-            print("FLIGHT TIME ERROR\n\n")
-            results['flight_time_err']['total'] += 1
-            accept, reason, error, inference_time = onboard_task_admission(model, max_flight, bat_perc, bat_health, link_qual, drone_state, flight_dur, task_dur)
-            inference_times.append(inference_time)
-            print(f"Inference time: {inference_time:.2f}")
-            print(f"{reason}\n\n")
-            if not accept and not error:
-                results['flight_time_err']['correct'] += 1
+            test_helper(model, t, results, 'flight_time_err', inference_times)
             continue
 
         if drone_state_ok and link_qual_ok and flight_ok and results['accept']['total'] < TEST_NUM:
-            print("======================================\n\n")
-            print(f"""
-                max_flight_time: {max_flight}
-                battery_percentage: {bat_perc}
-                battery_health: {bat_health}
-                link_quality: {link_qual}
-                drone_state: {drone_state}
-                flight_duration: {flight_dur}
-                task_duration: {task_dur}
-                """)
-            print("EXECUTABLE MISSION\n\n")
-            results['accept']['total'] += 1
-            accept, reason, error, inference_time = onboard_task_admission(model, max_flight, bat_perc, bat_health, link_qual, drone_state, flight_dur, task_dur)
-            inference_times.append(inference_time)
-            print(f"Inference time: {inference_time:.2f}")
-            print(f"{reason}\n\n")
-            if accept and not error:
-                results['accept']['correct'] += 1
+            test_helper(model, t, results, 'accept', inference_times)
 
     for item, value in results.items():
         print(f"{item} total: {value['total']} correct: {value['correct']}")
